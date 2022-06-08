45b438143a9a0b788655f433504d2ddf7190b63c
Raoulnest <nestorratongalaza1@gmail.com>
2022-06-08 05:54:43 +0300

    Creation des models

---
+++ app/Models/User_a_assister_user_a_assisters.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class User_a_assister_user_a_assisters extends Model
+{
+    use HasFactory;
+}
---
+++ database/migrations/2022_06_08_025303_create_probleme_a_assisters_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProblemeAAssistersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('probleme_a_assisters', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('probleme_a_assisters');
+    }
+}
